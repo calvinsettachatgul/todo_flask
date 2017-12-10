27917838266713a29613d3ef7844cd7a3e4b5c9c
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2017-12-09 19:12:12 -0800

    rendering todo list from json file

--- todo.py
+++ todo.py
@@ -3,23 +3,24 @@
 from flask import Flask
 from flask import render_template
 from pprint import pprint
+import json 
 
 app = Flask(__name__)
 
-all_todos = [
-                "Python",   
-                "Ruby", 
-                "Javasript"
-            ]
-
 def get_todos():
-    return  all_todos
+    todos = {}
+    with open('./data/todos.json') as json_data:
+        d = json.load(json_data)
+        json_data.close()
+        print(d["todos"])
+        todos = d["todos"]
+    return  todos
 
 @app.route('/')
 def index():
     todos = get_todos()
     print(todos)
-    return 'All Todos'
+    return render_template('index.html', todos=todos)
 
 @app.route("/welcome")
 def welcome(name="Calvin"):
